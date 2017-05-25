cbeea5e9dd4630892ff77605b0c9b384a8891263
Andrey Likhtar <andrey.likhtar@gmail.com>
2017-05-25 18:04:02 +0300

    Adding project to github

---
+++ src/main/java/com/artificial/model/Spitter.java
@@ -0,0 +1,68 @@
+package com.artificial.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by lav on 24.08.2016.
+ */
+@Entity
+@Table(name = "spitter")
+public class Spitter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "spitterId")
+    private int id;
+
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "fullname")
+    private String fullName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
---
+++ src/test/java/controller/HomeControllerTest.java
@@ -0,0 +1,8 @@
+package controller;
+
+/**
+ * Created by lav on 24.08.2016.
+ */
+
+public class HomeControllerTest {
+}
